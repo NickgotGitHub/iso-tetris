dd959fc8b953c908dd0db5eecd2b568f1b0ffeae
NickgotGitHub <sir.nsalmon@gmail.com>
2025-06-24 07:48:20 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   isom-tetris/.gitignore
    	new file:   isom-tetris/capacitor.config.ts
    	new file:   isom-tetris/index.html
    	new file:   isom-tetris/package-lock.json
    	new file:   isom-tetris/package.json
    	new file:   isom-tetris/public/vite.svg
    	new file:   isom-tetris/src/counter.ts
    	new file:   isom-tetris/src/main.ts
    	new file:   isom-tetris/src/style.css
    	new file:   isom-tetris/src/typescript.svg
    	new file:   isom-tetris/src/vite-env.d.ts
    	new file:   isom-tetris/tsconfig.json

---
+++ isom-tetris/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.isomtetris',
+  appName: 'IsomTetris',
+  webDir: 'dist'
+};
+
+export default config;
